df0500b09509acc9747af69a1b71c04a495dc9a3
Paul Rodriguez <paulyd0123@gmail.com>
2026-01-09 15:41:51 -0500

    created and added necessary functions for the tests in htmlHelper

---
+++ tests/helpers/constants.ts
@@ -0,0 +1,2 @@
+export const host = "http://localhost:5173/"
+export const waitTime = 3000 // milliseconds
---
+++ tests/helpers/htmlHelper.ts
@@ -0,0 +1,30 @@
+import { Builder, Browser, By, until, WebDriver } from "selenium-webdriver";
+import { host, waitTime } from "./constants"
+
+let driver: WebDriver;
+
+export async function buildDriver() {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    return driver;
+}
+
+export async function launchDriver() {
+    await driver.manage().window().maximize();
+    await driver.get(host);
+}
+
+export async function stopDriver() {
+    await driver.quit();
+}
+
+export async function getElementById(id: string) {
+    const element = await driver.wait(until.elementLocated(By.id(id)), waitTime);
+    return await driver.wait(until.elementIsVisible(element), waitTime);
+}
+
+export async function getElementByXPath(xpath: string) {
+    const element = await driver.wait(until.elementLocated(By.xpath(xpath)), waitTime);
+    return await driver.wait(until.elementIsVisible(element), waitTime);
+}
+
+ 
